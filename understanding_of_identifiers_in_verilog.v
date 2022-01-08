a6b0a1df851377a7655f5082bba4ed599564c74f
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2022-01-08 12:24:41 +0530

    Create understanding_of_identifiers_in_verilog.v
    Notes:- fun (name given to the module), inputs a,b, output y and gatenames A and O all indentfier provided from the user side.

---
+++ understanding_of_identifiers_in_verilog.v
@@ -0,0 +1,11 @@
+// module decleration.
+module fun(input a,b,output y);
+ //(Identifiers in this module are
+ //fun (name given to the module), inputs a,b, output y and gatenames A and O
+// all indentfier provided from the user side.) 
+  
+  and A (y,a,b);  // gate level design 
+  or O (y,a,b);
+  
+endmodule // endmodule.
+
