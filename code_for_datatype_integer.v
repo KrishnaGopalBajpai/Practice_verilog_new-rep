cbfd06fb74230c3815fd359275599ae35dcf968a
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2022-01-08 19:10:25 +0530

    Create code_for_datatype_integer.v
    Integer :- It's default size 32 bit at least or host dependent. can't be declare by user.

---
+++ code_for_datatype_integer.v
@@ -0,0 +1,17 @@
+
+// integer decleration understanding.
+module integer_checks;
+    
+  integer i1;
+  integer [3:0] k2; // invalied syntex bcoz of inbult 32 bit.
+  
+  initial begin
+    i1=54;
+  end
+  
+endmodule
+
+
+
+// Error reported:- due to inbilt 32 bit atleast or machine dependent.
+//** Error: D:\verilog\understanding_of_datatype_integerchks.v(5): Can't have packed array of integer type.
