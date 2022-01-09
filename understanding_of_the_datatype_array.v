5dc6cc4e1a7d36e3c6096bee50bfc6abd26fdcab
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2022-01-09 16:06:09 +0530

    Update understanding_of_the_datatype_array.v
    // Array:- used to store same type of data together.
    //syntex:- <datatype> array_name<arraysize>

--- understanding_of_the_datatype_array.v
+++ understanding_of_the_datatype_array.v
@@ -1,8 +1,13 @@
+// Array:- used to store same type of data together.
+//syntex:- <datatype> array_name<arraysize>
 //undedtanding of the arrar data type.
 
 module arr_checks;
   reg[4:0]mem[7:0]; // (sim o/p:- fixed size array)array decleration
   reg[4:0]temp;  //(sim o/p:- Packed array)internal declaeration
+  intger numb [0:15];  // array od intger numbers.
+  wire temp [1:10][0:5];  // 2D array of wire type.
+  reg [3:0]mem[0:7][0:7];  // 2D memory decleration.
     
     initial begin  // procedural block
       mem[5]=4'b1101; // access 5th loc of memory also perform write operation.
